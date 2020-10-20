964c452a3683da986b6591a4f945f262ea839d52
Francis Bulu <Francis.Bulu@student.intecbrussel.be>
2020-10-20 13:13:11 +0200

    Add project files.

---
+++ LoopsRevisited/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsRevisited
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14"); // hard coded -> zeer slecht
+
+            #region Mine
+            //for (int i = 0; i < 14; i++)
+            //{
+            //    int j = i + 1;
+            //    Console.Write($"{j}, ");
+            //}
+            #endregion
+
+            // int starNumber = 1;
+            // int endNumber = 14;
+            Console.WriteLine("Geen een getal in voor het begin");
+            int starNumber = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Geen een getal in voor het eind");
+
+            int endNumber = Int32.Parse(Console.ReadLine());
+            while (starNumber <= endNumber)
+            {
+
+                Console.WriteLine(starNumber);
+                starNumber+=3; // --> starNumber = starNumber + 1;
+            }
+        }
+    }
+}
